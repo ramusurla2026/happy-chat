942d83b0e8c66298165fa39a234227db0ebdfb91
Ramu Surla <surlaramu672@gmail.com>
2026-08-11 17:56:56 +0530

    Update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.happychats.in',
+  apiUrl: 'https://api.happychats.in/api',
   socketUrl: 'https://circle-up-fk10.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://circle-up-fk10.onrender.com/api',
-  apiUrl: 'https://api.happychats.in',
+  apiUrl: 'https://api.happychats.in/api',
   socketUrl: 'https://circle-up-fk10.onrender.com'
 };
 
