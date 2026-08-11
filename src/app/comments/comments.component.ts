7b759de5053a0b7a74271c934a44593455868d0b
Ramu Surla <surlaramu672@gmail.com>
2026-08-12 01:07:53 +0530

    re write comment code

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,28 +1,35 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, ModalController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonIcon,
+  IonFooter
+} from '@ionic/angular/standalone';
 
 import { Api } from '../core/services/api';
 import { Feed } from '../core/services/feed';
 
 import {
-  send,
   close,
   heart,
   heartOutline,
   chatbubbleOutline,
   paperPlaneOutline
 } from 'ionicons/icons';
+
 import { addIcons } from 'ionicons';
 
 addIcons({
-  'heart-outline': heartOutline,
-  'send': send,
-
   'close': close,
-  paperPlaneOutline
-
+  'heart': heart,
+  'heart-outline': heartOutline,
+  'chatbubble-outline': chatbubbleOutline,
+  'paper-plane-outline': paperPlaneOutline
 });
 
 @Component({
@@ -32,7 +39,12 @@ addIcons({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule
+
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonIcon,
+    IonFooter
   ],
 
   templateUrl: './comments.component.html',
@@ -40,11 +52,9 @@ addIcons({
 })
 export class CommentsComponent implements OnInit {
 
-
   @Input() postId!: string;
   @Input() type: string = 'post';
   @Input() profileImage: string = '';
-
 
   comments: any[] = [];
 
@@ -55,152 +65,107 @@ export class CommentsComponent implements OnInit {
 
   myProfileImage = '';
 
-
   constructor(
     private api: Api,
-    private modalCtrl: ModalController, private feedService: Feed
-  ) { }
+    private modalCtrl: ModalController,
+    private feedService: Feed
+  ) {}
 
-
-
-  ngOnInit() {
-
+  ngOnInit(): void {
     this.contentId = this.postId;
     this.contentType = this.type;
     this.myProfileImage = this.profileImage;
+
     this.getComments();
   }
 
-
-  close() {
+  close(): void {
     this.modalCtrl.dismiss();
   }
 
+  getComments(): void {
 
-
-
-
-  getComments() {
-    let url =
+    const url =
       this.contentType === 'reel'
-        ?
-        `/reels/${this.contentId}/comments`
-        :
-        `/posts/${this.contentId}/comments`;
+        ? `/reels/${this.contentId}/comments`
+        : `/posts/${this.contentId}/comments`;
 
+    this.api.get<any>(url).subscribe({
 
+      next: (res) => {
+        this.comments = res.data?.comments || [];
+      },
 
-    this.api.get<any>(url)
-      .subscribe({
-        next: (res) => {
-          this.comments =
-            res.data.comments || [];
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      });
+      error: (err) => {
+        console.log('Get comments error:', err);
+      }
+
+    });
   }
 
+  addComment(): void {
 
-
-
-
-
-  addComment() {
-
-    if (!this.text.trim())
+    if (!this.text.trim()) {
       return;
-    let url =
+    }
+
+    const url =
       this.contentType === 'reel'
-        ?
-        `/reels/${this.contentId}/comments`
-        :
-        `/posts/${this.contentId}/comments`;
-
-
-
+        ? `/reels/${this.contentId}/comments`
+        : `/posts/${this.contentId}/comments`;
 
     this.api.post<any>(
       url,
       {
         text: this.text
       }
-    )
-      .subscribe({
+    ).subscribe({
 
-        next: (res) => {
+      next: (res) => {
 
+        this.comments.push(res.data);
 
-          this.comments.push(res.data);
+        this.text = '';
 
+        this.feedService.updateCommentCount(
+          this.contentId,
+          this.comments.length
+        );
 
-          this.text = '';
+      },
 
-          this.feedService.updateCommentCount(
-            this.contentId,
-            this.comments.length
-          );
+      error: (err) => {
+        console.log('Add comment error:', err);
+      }
 
-
-        },
-
-        error: (err) => {
-
-          console.log(err);
-
-        }
-
-      })
-
-
+    });
   }
 
-
-  likeComment(comment: any) {
+  likeComment(comment: any): void {
 
     const url =
       this.contentType === 'reel'
         ? `/reels/${this.contentId}/comments/${comment.id}/like`
         : `/posts/${this.contentId}/comments/${comment.id}/like`;
 
-    this.api.post<any>(url, {})
-      .subscribe({
+    this.api.post<any>(url, {}).subscribe({
 
-        next: (res) => {
+      next: (res) => {
 
-          comment.hasLiked = res.data.liked;
+        comment.hasLiked = res.data.liked;
+        comment.likeCount = res.data.likeCount;
 
-          comment.likeCount = res.data.likeCount;
+      },
 
-        },
+      error: (err) => {
+        console.log('Like comment error:', err);
+      }
 
-        error: (err) => {
-
-          console.log(err);
-
-        }
-
-      });
-
+    });
   }
 
-
-
-
-
-
-  replyTo(comment: any) {
-
-    console.log(
-      "reply",
-      comment
-    );
-
+  replyTo(comment: any): void {
+    console.log('reply', comment);
   }
-
-
-
-
 
 }
